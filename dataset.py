77b268224ea3699fa58d6abc243e69a27f154af8
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2018-09-29 17:46:17 -0600

    more data preprocessing; batched data loading; plot every epoch

--- dataset.py
+++ dataset.py
@@ -1,46 +1,165 @@
 import json
+import random
+from typing import Type, List, Dict
 
 from torch.utils.data import Dataset, DataLoader
-import numpy as np
 import pandas as pd
 import torch
 
 
 class PipelineDataset(Dataset):
 
-    def __init__(self, path, device):
-        self.data = json.load(open(path, "r"))
+    def __init__(
+        self, data: List[Dict], feature_label: str, target_label: str,
+        device: str
+    ):
+        self.data = data
+        self.feature_label = feature_label
+        self.target_label = target_label
         self.device = device
 
-    # decides what to do with inside of the brackets (e.g. dataset[0])
-    def __getitem__(self, item):
-        # other processing can be done here
-        x = torch.tensor(self.data[item]["metafeatures"], dtype=torch.float32, device=self.device)
-        y = torch.tensor(self.data[item]["test_accuracy"], dtype=torch.float32, device=self.device)
-        pipeline = self.data[item]["job_str"].split("___")[1:]
-        return x, y, pipeline
+    def __getitem__(self, item:int):
+        x = torch.tensor(
+            self.data[item][self.feature_label],
+            dtype=torch.float32,
+            device=self.device
+        )
+        y = torch.tensor(
+            self.data[item][self.target_label],
+            dtype=torch.float32,
+            device=self.device
+        )
+        return x, y
 
-    # defines how big the dataset is. Return a smaller value (either some fraction or a hard coded number of
-    # data points) if you want to get a train dataset.
     def __len__(self):
         return len(self.data)
 
 
-def preprocess_data(path):
+def _preprocess_data(path):
     data = json.load(open(path, "r"))
+
     metafeatures = []
     for pipeline in data:
         metafeatures.append(pipeline["metafeatures"])
     mf_df = pd.DataFrame(metafeatures)
     non_time_cols = [col for col in mf_df.columns if not "time" in col.lower()]
-    mf_df = mf_df[non_time_cols]
-    processed_mf_df = mf_df.dropna(axis=1, how="any")
-    processed_data = []
-    for pipeline, mfs in zip(data, processed_mf_df.values.tolist()):
-        processed_data.append({
-            "job_str": pipeline["job_str"],
-            "metafeatures": mfs,
-            "test_accuracy": pipeline["test_accuracy"]
-        })
-    json.dump(processed_data, open("./data/processed_data.json", "w"), indent=4)
+    mf_df = mf_df[non_time_cols].replace([float("inf"), -float("inf")], float("nan"))
+    mf_df = mf_df.dropna(axis=1, how="any")
+    processed_mf_df = (mf_df - mf_df.mean()) / mf_df.std()
 
+    processed_data = []
+    for pipeline_data, mfs in zip(data, processed_mf_df.values.tolist()):
+        dataset, pipeline = pipeline_data["job_str"].split("___", 1)
+        processed_data.append({
+            "dataset": dataset,
+            "pipeline": pipeline,
+            "metafeatures": mfs,
+            "train_accuracy": pipeline_data["train_accuracy"],
+            "test_accuracy": pipeline_data["test_accuracy"],
+            "train_time": pipeline_data["train_fit_time"] +\
+                pipeline_data["train_predict_time"],
+            "test_time": pipeline_data["test_predict_time"]
+        })
+    json.dump(
+        processed_data, open("./data/processed_data.json", "w"), indent=4
+    )
+
+
+class GroupDataLoader(object):
+    """
+    Batches a dataset for PyTorch Neural Network training. Partitions the
+    dataset so that batches belong to the same group.
+    Parameters:
+    -----------
+    path: str, path to JSON file containing pipeline run data. file must
+        contain an array of objects.
+    group_label: str, pipeline run data is grouped by group_label and each
+        batch of data comes from only one group. group_label must be a key into
+        each element of the pipeline run data. the value of group_label must be
+        hashable.
+    dataset_class: Type[torch.utils.data.Dataset], the class used to make
+        dataset instances after the dataset is partitioned.
+    dataset_params: dict, extra parameters needed to instantiate dataset_class
+    batch_size: int, the number of data points in each batch
+    drop_last: bool, default False. whether to drop the last incomplete batch.
+    shuffle: bool, default True. whether to randomize the batches.
+    seed: int, default 0. random seed.
+    """
+
+    def __init__(
+        self, path: str, group_label: str, dataset_class: Type[Dataset],
+        dataset_params: dict, batch_size: int, drop_last: bool = False,
+        shuffle: bool = True, device: str = "cpu", seed: int = 0
+    ):
+        self.path = path
+        self.group_label = group_label
+        self.dataset_class = dataset_class
+        self.dataset_params = dataset_params
+        self.batch_size = batch_size
+        self.drop_last = drop_last
+        self.shuffle = shuffle
+        self.device = device
+        self.seed = seed
+
+        self._load_data()
+        self._init_dataloaders()
+        self._init_group_metadataloader()
+
+    def _load_data(self):
+        """
+        Reads the dataset from self.path.
+        """
+        with open(self.path, "r") as f:
+            self._data = json.load(f)
+
+    def _init_dataloaders(self):
+        """
+        Groups self._data based on group_label. Creates a
+        torch.utils.data.DataLoader for each group, using self.dataset_class.
+        """
+        # group the data
+        grouped_data = {}
+        for data_point in self._data:
+            group = data_point[self.group_label]
+            if not group in grouped_data:
+                grouped_data[group] = []
+            grouped_data[group].append(data_point)
+
+        # create dataloaders
+        self._group_dataloaders = {}
+        for group, group_data in grouped_data.items():
+            group_dataset = self.dataset_class(group_data, **self.dataset_params)
+            self._group_dataloaders[group] = DataLoader(
+                dataset=group_dataset,
+                batch_size=self.batch_size,
+                shuffle=self.shuffle,
+                drop_last=self.drop_last
+            )
+
+    def _init_group_metadataloader(self):
+        group_batches = []
+        for group, group_dataloader in self._group_dataloaders.items():
+            group_batches += [group] * len(group_dataloader)
+        self._group_metadataloader = DataLoader(
+            dataset=group_batches,
+            batch_size=1,
+            shuffle=self.shuffle
+        )
+
+    def __iter__(self):
+        group_dataloader_iters = {}
+        for group in self._group_metadataloader:
+            group = group[0]
+            if not group in group_dataloader_iters:
+                group_dataloader_iters[group] = iter(
+                    self._group_dataloaders[group]
+                )
+            yield group, next(group_dataloader_iters[group])
+        raise StopIteration()
+
+    def __len__(self):
+        return len(self._group_metadataloader)
+
+
+if __name__ == '__main__':
+    _preprocess_data("./data/complete_pipelines_and_metafeatures.json")
--- train_pipeline_model.py
+++ train_pipeline_model.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import torch
 from tqdm import tqdm
 
-from dataset import PipelineDataset, preprocess_data
+from dataset import PipelineDataset, GroupDataLoader
 from models import PrimitiveModel, AccuracyRegressionModel
 
 
@@ -16,13 +16,15 @@ input_model = nn.Module
 output_model = nn.Module
 
 
-def instantiate_submodels(primitive_names, mf_len):
+def instantiate_submodels(pipeline, mf_len):
+    primitive_names = pipeline.split("___")
     for primitive_name in primitive_names:
         if not primitive_name in primitive_submodel_dict:
             primitive_submodel_dict[primitive_name] = PrimitiveModel(mf_len)
 
 
-def build_model(primitive_names, mf_len):
+def build_model(pipeline, mf_len):
+    primitive_names = pipeline.split("___")
     primitive_models = []
     primitive_models.append(input_model)
     for primitive_name in primitive_names:
@@ -30,40 +32,52 @@ def build_model(primitive_names, mf_len):
     primitive_models.append(output_model)
 
     return nn.Sequential(*primitive_models)
-    
+
 
 def main():
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda:0" if use_cuda else "cpu")
 
-    pipelines = PipelineDataset("./data/processed_data.json", device)
-    mf_len = len(pipelines[0][0])
+    dataloader = GroupDataLoader(
+        path = "./data/processed_data.json",
+        group_label = "pipeline",
+        dataset_class = PipelineDataset,
+        dataset_params = {
+            "feature_label": "metafeatures",
+            "target_label": "test_accuracy",
+            "device": "cuda:0"
+        },
+        batch_size = 48
+    )
+    input_size = next(iter(dataloader))[1][0].shape[1]
+
 
     global input_model
-    input_model = PrimitiveModel(mf_len)
+    input_model = PrimitiveModel(input_size)
     global output_model
-    output_model = AccuracyRegressionModel(mf_len)
+    output_model = AccuracyRegressionModel(input_size)
 
-    objective = torch.nn.MSELoss()
-    n_epochs = 5
+    objective = torch.nn.MSELoss(reduction="elementwise_mean")
+    n_epochs = 5000
     # loop = tqdm(total=n_epochs, position=0)
     losses = []
+    all_epoch_losses = []
     for e, epoch in enumerate(range(n_epochs)):
         # np.random.shuffle(pipelines)
         # loop = tqdm(total=len(pipelines), position=0)
+        epoch_losses = []
+        for i, (pipeline, (x_batch, y_batch)) in enumerate(dataloader):
+            instantiate_submodels(pipeline, input_size)
 
-        for i, (x, y, primitive_names) in enumerate(pipelines):
-            instantiate_submodels(primitive_names, mf_len)
-            
-            pipeline_regressor = build_model(primitive_names, mf_len)
+            pipeline_regressor = build_model(pipeline, input_size)
             pipeline_regressor.cuda()
 
             optimizer = optim.Adam(pipeline_regressor.parameters(), lr=1e-4)
 
             optimizer.zero_grad()
 
-            prediction = pipeline_regressor(x)
-            loss = torch.sqrt(objective(prediction, y))
+            y_hat_batch = torch.squeeze(pipeline_regressor(x_batch))
+            loss = torch.sqrt(objective(y_hat_batch, y_batch))
 
             # loop.set_description('loss:{:.4f}'.format(loss.item()))
             # loop.update(1)
@@ -71,25 +85,36 @@ def main():
             loss.backward()
             optimizer.step()
 
-            if (i+1) % 100 == 0:
-                print(e, n_epochs, i, len(pipelines), loss.item())
+            epoch_losses.append(loss.item())
             losses.append(loss.item())
 
+            print(e, n_epochs, i, len(dataloader), loss.item())
+
+        all_epoch_losses.append(epoch_losses)
         # print progress of loss
-        
-    # loop.close()
 
-    plt.plot(losses)
-    plt.xlabel("training iteration")
-    plt.ylabel("rmse loss")
-    plt.title("Pipeline Regressor Training Loss")
-    plt.show()
+        # loop.close()
 
-    plt.hist(losses, bins=20, range=(0,1))
-    plt.show()
+        plt.plot(losses)
+        plt.xlabel("training iteration")
+        plt.ylabel("rmse loss")
+        plt.title("Pipeline Regressor Training Loss")
+        plt.savefig("all_losses.png")
+        plt.clf()
+
+        avg_epoch_losses = list(map(lambda x: np.mean(x), all_epoch_losses))
+        plt.plot(avg_epoch_losses)
+        plt.xlabel("epoch")
+        plt.ylabel("avg rmse loss")
+        plt.ylim((0,1))
+        plt.title("Pipeline Regressor Average Training Loss")
+        plt.savefig("avg_losses.png")
+        plt.clf()
+
+        plt.hist(losses, bins=20, range=(0,1))
+        plt.savefig("loss_hist.png")
+        plt.clf()
 
 
 if __name__ == "__main__":
     main()
-    # preprocess_data("./data/complete_pipelines_and_metafeatures.json")
-
