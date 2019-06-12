9226aef453cd183164b0ae43cc1054576c4dba39
Erik Duane Huckvale <erikpetr@smith.aml.cs.byu.edu>
2019-06-12 11:43:26 -0600

    DNA dropout can now be adjusted in the config file; Dropout does NOT affect determinism in the DNA

--- dna/models.py
+++ dna/models.py
@@ -81,8 +81,8 @@ class DNAModule(nn.Module):
 
     def __init__(
         self, submodule_input_sizes: typing.Dict[str, int], n_layers: int, input_layer_size: int, hidden_layer_size: int,
-        output_layer_size: int, activation_name: str, use_batch_norm: bool, use_skip: bool = False, *,
-        device: str = 'cuda:0', seed: int = 0
+        output_layer_size: int, activation_name: str, use_batch_norm: bool, use_skip: bool = False, dropout: float = 0.0,
+        *, device: str = 'cuda:0', seed: int = 0
     ):
         super(DNAModule, self).__init__()
         self.submodule_input_sizes = submodule_input_sizes
@@ -94,6 +94,7 @@ class DNAModule(nn.Module):
         self._activation = F_ACTIVATIONS[activation_name]
         self.use_batch_norm = use_batch_norm
         self.use_skip = use_skip
+        self.dropout = dropout
         self.device = device
         self.seed = seed
         self._input_seed = seed + 1
@@ -106,14 +107,14 @@ class DNAModule(nn.Module):
     def _get_input_submodule(self):
         layer_sizes = [self.input_layer_size] + [self.hidden_layer_size] * (self.n_layers - 1)
         return Submodule(
-            layer_sizes, self.activation_name, self.use_batch_norm, self.use_skip, device=self.device,
+            layer_sizes, self.activation_name, self.use_batch_norm, self.use_skip, self.dropout, device=self.device,
             seed=self._input_seed
         )
 
     def _get_output_submodule(self):
         layer_sizes = [self.hidden_layer_size] * (self.n_layers - 1) + [self.output_layer_size]
         return Submodule(
-            layer_sizes, self.activation_name, self.use_batch_norm, self.use_skip, device=self.device,
+            layer_sizes, self.activation_name, self.use_batch_norm, self.use_skip, self.dropout, device=self.device,
             seed=self._output_seed
         )
 
@@ -122,7 +123,7 @@ class DNAModule(nn.Module):
         for i, (submodule_id, submodule_input_size) in enumerate(sorted(self.submodule_input_sizes.items())):
             layer_sizes = [self.hidden_layer_size * submodule_input_size] + [self.hidden_layer_size] * (self.n_layers - 1)
             dynamic_submodules[submodule_id] = Submodule(
-                layer_sizes, self.activation_name, self.use_batch_norm, self.use_skip, device=self.device,
+                layer_sizes, self.activation_name, self.use_batch_norm, self.use_skip, self.dropout, device=self.device,
                 seed=self._dna_base_seed + i
             )
         return dynamic_submodules
@@ -342,7 +343,7 @@ class DNARegressionModel(PyTorchRegressionRankModelBase):
 
     def __init__(
         self, n_hidden_layers: int, hidden_layer_size: int, activation_name: str, use_batch_norm: bool,
-        use_skip: bool = False, *, device: str = 'cuda:0', seed: int = 0
+        use_skip: bool = False, dropout = 0.0, *, device: str = 'cuda:0', seed: int = 0
     ):
         PyTorchModelBase.__init__(self, y_dtype=torch.float32, device=device, seed=seed)
         RegressionModelBase.__init__(self, seed=seed)
@@ -353,6 +354,7 @@ class DNARegressionModel(PyTorchRegressionRankModelBase):
         self.activation_name = activation_name
         self.use_batch_norm = use_batch_norm
         self.use_skip = use_skip
+        self.dropout = dropout
         self.output_layer_size = 1
         self._model_seed = self.seed + 1
 
@@ -365,8 +367,8 @@ class DNARegressionModel(PyTorchRegressionRankModelBase):
 
         return DNAModule(
             submodule_input_sizes, self.n_hidden_layers + 1, self.input_layer_size, self.hidden_layer_size,
-            self.output_layer_size, self.activation_name, self.use_batch_norm, self.use_skip, device=self.device,
-            seed=self._model_seed
+            self.output_layer_size, self.activation_name, self.use_batch_norm, self.use_skip, self.dropout,
+            device=self.device, seed=self._model_seed
         )
 
     def _get_loss_function(self):
