0f8145e5ef2025696b861994868d6836462a2652
Erik Duane Huckvale <erikpetr@smith.aml.cs.byu.edu>
2019-07-08 13:11:56 -0600

    Enabled dna to be installed; wrote a script to install dna and run the tests using it as an installed package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+from dna import __version__
+
+setup(name='dna',
+      packages=find_packages(include=['dna', 'dna.*']),
+      version=__version__,
+      description='A collection of models used for pipeline selection',
+      author='Brandon Schoenfeld, Orion Weller, Mason Poggemann, Even Peterson, Erik Huckvale',
+      url='https://github.com/byu-dml/d3m-dynamic-neural-architecture/tree/develop/dna',
+      include_package_data=True,
+      keywords=['metalearning', 'metafeature', 'machine learning', 'metalearn', 'dna'],
+      install_requires=[
+            'pandas==0.24.2',
+            'scikit-learn==0.21.1',
+            'scipy==1.3.0',
+            'sklearn==0.0',
+            'torch==1.1.0',
+            'tqdm==4.32.1'
+      ])
