15226da4c92eda0ccd24694352fec358d79f0805
Erik Duane Huckvale <erikpetr@smith.aml.cs.byu.edu>
2019-07-29 11:46:49 -0600

    Finished phase 1: the attention regression model

--- dna/models/attention_regression_model.py
+++ dna/models/attention_regression_model.py
@@ -1,1 +1,41 @@
-from .base_models import
+from .base_models import RNNRegressionRankSubsetModelBase
+from .torch_modules.attention_mlp import AttentionMLP
+
+
+class AttentionRegressionModel(RNNRegressionRankSubsetModelBase):
+
+    def __init__(
+        self, n_layers: int, n_heads: int, dim_keys, dim_values, reduction: str, activation_name: str,
+        dropout: float,  output_n_hidden_layers: int, output_hidden_layer_size: int, use_batch_norm: bool,
+        use_skip: bool, *, device: str = 'cuda:0', seed: int = 0
+    ):
+
+        super().__init__(activation_name, dropout, output_n_hidden_layers, output_hidden_layer_size, use_batch_norm,
+                         use_skip, device=device, seed=seed)
+
+        self.n_layers = n_layers
+        self.n_heads = n_heads
+        self.dim_keys = dim_keys
+        self.dim_values = dim_values
+        self.reduction = reduction
+
+    def _get_model(self, train_data):
+        n_features = len(train_data[0][self.features_key])
+        return AttentionMLP(
+            n_layers=self.n_layers,
+            n_heads=self.n_heads,
+            dim_model=self.num_primitives,
+            dim_keys=self.dim_keys,
+            dim_values=self.dim_values,
+            dropout=self.dropout,
+            reduction=self.reduction,
+            mlp_extra_input_size=n_features,
+            mlp_hidden_layer_size=self.output_hidden_layer_size,
+            mlp_n_hidden_layers=self.output_n_hidden_layers,
+            mlp_activation_name=self.activation_name,
+            output_size=1,
+            mlp_use_batch_norm=self.use_batch_norm,
+            mlp_use_skip=self.use_skip,
+            device=self.device,
+            seed=self._model_seed
+        )
--- dna/models/torch_modules/attention_mlp.py
+++ dna/models/torch_modules/attention_mlp.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from transformer import Encoder
 
+from . import PyTorchRandomStateContext
 from .submodule import Submodule
 from dna.utils import get_reduction_function
 
@@ -14,28 +15,32 @@ class AttentionMLP(nn.Module):
     """
 
     def __init__(
-            self, n_layers: int, n_heads: int, dim_model: int, dim_keys, dim_values, dropout: float, reduction: str,
-            mlp_extra_input_size: int, mlp_hidden_layer_size: int, mlp_n_hidden_layers: int, mlp_activation_name: str,
-            output_size: int, mlp_use_batch_norm: bool, mlp_use_skip: bool, *, device: str, seed: int
+        self, n_layers: int, n_heads: int, dim_model: int, dim_keys, dim_values, dropout: float, reduction: str,
+        mlp_extra_input_size: int, mlp_hidden_layer_size: int, mlp_n_hidden_layers: int, mlp_activation_name: str,
+        output_size: int, mlp_use_batch_norm: bool, mlp_use_skip: bool, *, device: str, seed: int
     ):
 
         super().__init__()
 
         self.reduction = get_reduction_function(reduction)
         dimensions = (dim_model, dim_keys, dim_values)
-        self.attention = Encoder(n_layers, n_heads, *dimensions, dropout, dropout, 0)
+
+        with PyTorchRandomStateContext(seed=seed):
+            self.attention = Encoder(n_layers, n_heads, *dimensions, dropout, dropout, 0)
+        self.attention = self.attention.to(device)
 
         mlp_input_size = dim_model + mlp_extra_input_size
         mlp_layer_sizes = [mlp_input_size] + [mlp_hidden_layer_size] * mlp_n_hidden_layers + [output_size]
-        self.mlp = Submodule(mlp_layer_sizes, mlp_activation_name, mlp_use_batch_norm, mlp_use_skip, dropout,
-                             device=device, seed=seed+1)
+        self.mlp = Submodule(
+            mlp_layer_sizes, mlp_activation_name, mlp_use_batch_norm, mlp_use_skip, dropout, device=device, seed=seed+1
+        )
 
     def forward(self, args):
         sequence, features = args
-        sequence = self.attention(sequence)
-        encoded_sequence = self.reduction(
-            torch.stack(sequence, dim=0)
-        )
+        attended_sequence = self.attention(sequence)
+
+        seq_len_dim = 1
+        encoded_sequence = self.reduction(attended_sequence, dim=seq_len_dim)
         mlp_input = torch.cat((encoded_sequence, features), dim=1)
         output = self.mlp(mlp_input)
         return output.squeeze()
