a2c2337570e05d8793f8e7098f317edfb2931fb5
Erik Duane Huckvale <erikpetr@smith.aml.cs.byu.edu>
2019-08-02 10:33:19 -0600

    Fixed bug

--- dna/models/attention_regression_model.py
+++ dna/models/attention_regression_model.py
@@ -6,7 +6,7 @@ class AttentionRegressionModel(RNNRegressionRankSubsetModelBase):
 
     def __init__(
         self, n_layers: int, n_heads: int, attention_in_features: int, attention_hidden_features: int,
-        attention_activation_name: str, reduction_name: str, activation_name: str, dropout: float,
+        attention_activation_name: str, reduction_name: str, use_mask: bool, activation_name: str, dropout: float,
         output_n_hidden_layers: int, output_hidden_layer_size: int, use_batch_norm: bool, use_skip: bool, *,
         device: str = 'cuda:0', seed: int = 0
     ):
@@ -21,6 +21,7 @@ class AttentionRegressionModel(RNNRegressionRankSubsetModelBase):
         self.attention_hidden_features = attention_hidden_features
         self.attention_activation_name = attention_activation_name
         self.reduction_name = reduction_name
+        self.use_mask = use_mask
 
     def _get_model(self, train_data):
         n_features = len(train_data[0][self.features_key])
@@ -33,6 +34,7 @@ class AttentionRegressionModel(RNNRegressionRankSubsetModelBase):
             attention_hidden_features=self.attention_hidden_features,
             dropout=self.dropout,
             reduction_name=self.reduction_name,
+            use_mask=self.use_mask,
             mlp_extra_input_size=n_features,
             mlp_hidden_layer_size=self.output_hidden_layer_size,
             mlp_n_hidden_layers=self.output_n_hidden_layers,
