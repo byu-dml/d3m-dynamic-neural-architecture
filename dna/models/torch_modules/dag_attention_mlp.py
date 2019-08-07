754b38ffd068f3a6059294867b074723fa9c2b59
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2019-08-07 15:32:56 -0600

    updated docstring; formatting

--- dna/models/torch_modules/dag_attention_mlp.py
+++ dna/models/torch_modules/dag_attention_mlp.py
@@ -3,25 +3,22 @@ from .attention_mlp import AttentionMLP
 
 class DAGAttentionMLP(AttentionMLP):
     """
-    The DAG Attention MLP, like the Attention MLP which it extends, encodes a sequence and passes it through an MLP
-    along with some additional concatenated features. But the sequence is treated like a dag, utilizing a dag structure
-    passed into the forward function. The structures of the dags that this model processes not only define the immediate
-    inputs to a given node, but all the inputs that come before it. This way, attention can be applied to the node
-    itself and all the nodes it depends on. The structure is still ambiguous, however. The ambiguity is each node is
-    defined by a collection including itself and all its dependents. But this collection does not take into account the
-    various paths of dependents leading to that node, beginning at a start node.
+    The DAG Attention MLP encodes a DAG, concatenates this encoding with an additional feature vector, and passes
+    the concatenated vector to a final MLP. For each node, attention is applied to that node and only the nodes on
+    which it depends, as a sequence.
     """
 
     def __init__(
         self, n_layers: int, n_heads: int, in_features: int, attention_in_features: int, attention_hidden_features,
-        attention_activation_name: str, dropout: float, reduction_name: str, use_mask: bool, mlp_extra_input_size: int,
-        mlp_hidden_layer_size: int, mlp_n_hidden_layers: int, mlp_activation_name: str, output_size: int,
-        mlp_use_batch_norm: bool, mlp_use_skip: bool, *, device: str, seed: int
+        attention_activation_name: str, dropout: float, reduction_name: str, use_mask: bool,
+        mlp_extra_input_size: int, mlp_hidden_layer_size: int, mlp_n_hidden_layers: int, mlp_activation_name: str,
+        output_size: int, mlp_use_batch_norm: bool, mlp_use_skip: bool, *, device: str, seed: int
     ):
         super().__init__(
-            n_layers, n_heads, in_features, attention_in_features, attention_hidden_features, attention_activation_name,
-            dropout, reduction_name, use_mask, mlp_extra_input_size, mlp_hidden_layer_size, mlp_n_hidden_layers,
-            mlp_activation_name, output_size, mlp_use_batch_norm, mlp_use_skip, device=device, seed=seed
+            n_layers, n_heads, in_features, attention_in_features, attention_hidden_features,
+            attention_activation_name, dropout, reduction_name, use_mask, mlp_extra_input_size,
+            mlp_hidden_layer_size, mlp_n_hidden_layers, mlp_activation_name, output_size, mlp_use_batch_norm,
+            mlp_use_skip, device=device, seed=seed
         )
 
     def forward(self, args):
