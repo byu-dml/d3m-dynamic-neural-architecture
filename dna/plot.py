3a771197650cb4ecddf8ddab5794fab7904e3a87
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2019-10-23 11:30:10 -0600

    use model names and colors in violin plots

--- dna/plot.py
+++ dna/plot.py
@@ -8,6 +8,8 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 
+from dna.models import get_model_class
+
 
 def plot_at_k_scores_over_k(
     model_names: pd.Series, model_scores, model_errors, model_colors, plot_path, ylabel=None, title=None, max_k=100
@@ -29,9 +31,9 @@ def plot_at_k_scores_over_k(
     plt.clf()
 
 
-def plot_violin_of_score_distributions(scores_by_model, score_name, plot_path):
+def plot_violin_of_score_distributions(scores_by_model, score_name, model_colors, plot_path):
     # todo: use model names and colors
-    ax = sns.violinplot(data=pd.DataFrame(scores_by_model), cut=0)
+    ax = sns.violinplot(data=pd.DataFrame(scores_by_model), cut=0, palette=model_colors)
     plt.title('Distribution of {}'.format(score_name))
     plt.xlabel('Model')
     plt.ylabel('{}'.format(score_name))
@@ -56,8 +58,12 @@ def create_distribution_plots(results: pd.DataFrame, output_dir: str, list_of_k:
         else:
             scores_by_metric_by_model[metric_name][model_id].append(score_value)
 
+    model_colors = {}
+
     for row_number, row in results.iterrows():
         model_id = row['model_id']
+        model_class = get_model_class(model_id)
+        model_colors[model_class.name] = model_class.color
         for column_name, cell_value in row.iteritems():
             if column_name.startswith('test.scores_by_dataset_id'):
                 assert column_name.endswith('_by_run')
@@ -71,13 +77,13 @@ def create_distribution_plots(results: pd.DataFrame, output_dir: str, list_of_k:
                         assert type(cell_value) == list and type(cell_value[0]) == list
                         for run_scores_at_k in cell_value:
                             if k <= len(run_scores_at_k):
-                                insert_score(pretty_metric_name, model_id, run_scores_at_k[k-1]) # index 0 contains values for k=1
+                                insert_score(pretty_metric_name, model_class.name, run_scores_at_k[k-1]) # index 0 contains values for k=1
 
                 else:
-                    insert_score(metric_name, model_id, cell_value)
+                    insert_score(metric_name, model_class.name, cell_value)
 
     for metric_name, scores_by_model_id in scores_by_metric_by_model.items():
         plot_violin_of_score_distributions(
-            scores_by_model_id, metric_name,
+            scores_by_model_id, metric_name, model_colors,
             plot_path=os.path.join(output_dir, '{}_distributions.pdf'.format(metric_name))
         )
